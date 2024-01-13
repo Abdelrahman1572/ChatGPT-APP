c5246931e1cf567b99c932bf495da2f97a65ca36
Abdelrahman1572 <126156209+Abdelrahman1572@users.noreply.github.com>
2024-01-13 15:31:25 +0200

    Update Constants.dart

--- lib/shared/component/Constants.dart
+++ lib/shared/component/Constants.dart
@@ -1,1 +1,1 @@
-String openAI_API_Key = "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"; //enter your apikey from https://platform.openai.com/api-keys 
+String openAI_API_Key = ""; //enter your apikey from https://platform.openai.com/api-keys 
